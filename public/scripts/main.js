eb8993a6c0afe6694f53ab2bd0f514c5bc7207ea
kassiokennedy <kassio1206@hotmail.com>
2021-11-25 10:26:09 -0300

    25/11-10:26

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,14 @@
-import { Modal } from './modal'
+import Modal from './modal.js'
+
 const modal = Modal()
+
+//buttons of check classe
+const checkButtons = document.querySelectorAll('.actions a.check')
+
+checkButtons.forEach(button => {
+  button.addEventListener('click', event => {
+    //Abrir modal
+    modal.open()
+  })
+})
 //P
-modal.open()
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,6 +1,7 @@
-module.exports = function Modal() {
+export default function Modal() {
   function open() {
     //funcionalidade de atribuir classe active para a modal
+    document.querySelector('.modal-wrapper').classList.add('active')
   }
   function close() {
     //funcionalidade de remover classe active para a modal
