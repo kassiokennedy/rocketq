50a41602fef433e84a3e22c78069b3ea7275e433
kassiokennedy <kassio1206@hotmail.com>
2021-11-26 10:09:50 -0300

    26/11-10:09

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,13 +2,23 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
+const modalTittle = document.querySelector('.modal h2')
+const modalDiscription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 //buttons of check classe
 const checkButtons = document.querySelectorAll('.actions a.check')
 
 checkButtons.forEach(button => {
-  button.addEventListener('click', event => {
-    //Abrir modal
-    modal.open()
-  })
+  button.addEventListener('click', handleClick)
 })
-//P
+/* open modal when the delete button is clicked**/
+const deleteButton = document.querySelectorAll('.actions a.delete')
+
+deleteButton.forEach(button => {
+  button.addEventListener('click', handleClick)
+})
+function handleClick(event) {
+  //Open modal
+  modal.open()
+}
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,10 +1,15 @@
 export default function Modal() {
+  const cancelButton = document.querySelector('.button.cancel')
+  const modalWrapper = document.querySelector('.modal-wrapper')
+  cancelButton.addEventListener('click', close)
+
   function open() {
     //funcionalidade de atribuir classe active para a modal
-    document.querySelector('.modal-wrapper').classList.add('active')
+    modalWrapper.classList.add('active')
   }
   function close() {
     //funcionalidade de remover classe active para a modal
+    modalWrapper.classList.add('remove')
   }
 
   return {
