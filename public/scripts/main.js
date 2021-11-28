ecbe2e4fe8d3ecbc0b42635f14cd7581d45739da
kassiokennedy <kassio1206@hotmail.com>
2021-11-28 18:26:20 -0300

    28/11-18:26

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -16,9 +16,25 @@ checkButtons.forEach(button => {
 const deleteButton = document.querySelectorAll('.actions a.delete')
 
 deleteButton.forEach(button => {
-  button.addEventListener('click', handleClick)
+  button.addEventListener('click', event => handleClick(event, false))
 })
-function handleClick(event) {
+
+function handleClick(event, check = true) {
+  event.preventDefault()
+  const text = check ? 'Marcar como lida' : 'Excluir'
+  const slug = check ? 'check' : 'delete'
+  //get the 'id' of the room
+  const roomId = document.querySelector('#room-id').dataset.id
+  const questionId = event.target.dataset.id
+
+  const form = document.querySelector('.modal form')
+  form.setAttribute('action', `/room/${roomId}/${questionId}/${slug}`)
+
+  modalTittle.innerHTML = `${text} esta pergunta`
+  modalDiscription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta`
+  modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+  //change the delete button color
+  check ? modalButton.classList.remove('red') : modalButton.classList.add('red')
   //Open modal
   modal.open()
 }
